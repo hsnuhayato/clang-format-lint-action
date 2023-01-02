1dbb87f4133592e1b292a0a777c41049846c0d2f
MENGHUNG WU <m.wu@gitai.tech>
2023-01-03 00:47:23 +0900

    rename in watchdog

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,4 +3,4 @@
 cd "$GITHUB_WORKSPACE"
 
 /run-clang-format.py "$@" 2>&1 \
-    | reviewdog -efm="%f:%l: %m" -name="clang-format-cpplint"
+    | reviewdog -efm="%f:%l: %m" -name="clang-format-lint-action"
