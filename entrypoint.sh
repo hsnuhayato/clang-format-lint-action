c8471f70e901dda049c78d259b924bdc874a50ab
MENGHUNG WU <m.wu@gitai.tech>
2023-01-03 11:35:41 +0900

    file

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,4 +4,4 @@ cd "$GITHUB_WORKSPACE"
 export REVIEWDOG_GITHUB_API_TOKEN="${INPUT_GITHUB_TOKEN}"
 
 /run-clang-format.py "$@" 2>&1 \
-    | reviewdog -name="clang-format-lint-action" -filter-mode="git diff master" -f=diff
+    | reviewdog -name="clang-format-lint-action" -filter-mode=file -f=diff
