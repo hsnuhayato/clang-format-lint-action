0fc3284f0fea5c4e99ccc3cd76fe8d38a87526c6
MENGHUNG WU <m.wu@gitai.tech>
2023-01-03 18:50:23 +0900

    rm unnecessary comments

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,6 @@
 
 cd "$GITHUB_WORKSPACE"
 export REVIEWDOG_GITHUB_API_TOKEN="${INPUT_GITHUB_TOKEN}"
-#exit_val="0"
 
 /run-clang-format.py "$@" 2>&1 | \
     reviewdog -name="clang-format-lint-action" -f="diff" -tee \
@@ -11,8 +10,3 @@ export REVIEWDOG_GITHUB_API_TOKEN="${INPUT_GITHUB_TOKEN}"
               -fail-on-error="${INPUT_FAIL_ON_ERROR}" \
               -level="${INPUT_LEVEL}" \
               ${INPUT_REVIEWDOG_FLAGS}
-              #|| exit_val="$?"
-
-#if [[ "${exit_val}" -ne '0' ]]; then
-#  exit 1
-#fi
