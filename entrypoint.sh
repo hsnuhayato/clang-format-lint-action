56034fa0c0418729d54ffdff9510f1f5909d4e06
MENGHUNG WU <m.wu@gitai.tech>
2023-01-03 20:16:06 +0900

    hard coding reviewdog option

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,9 +4,9 @@ cd "$GITHUB_WORKSPACE"
 export REVIEWDOG_GITHUB_API_TOKEN="${INPUT_GITHUB_TOKEN}"
 
 /run-clang-format.py "$@" 2>&1 | \
-    reviewdog -name="clang-format-lint-action" -f="diff" -tee \
-              -reporter="${INPUT_REPORTER}" \
-              -filter-mode="${INPUT_FILTER_MODE}" \
-              -fail-on-error="${INPUT_FAIL_ON_ERROR}" \
-              -level="${INPUT_LEVEL}" \
+    reviewdog -name=clang-format-lint-action -f=diff -tee \
+              -reporter=github-pr-review \
+              -filter-mode=nofilter \
+              -fail-on-error=true \
+              -level=error \
               ${INPUT_REVIEWDOG_FLAGS}
