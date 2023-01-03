2920d6c670496af020abace1f30d1d1625c56dc5
MENGHUNG WU <m.wu@gitai.tech>
2023-01-03 15:31:33 +0900

    add args

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,16 @@
 
 cd "$GITHUB_WORKSPACE"
 export REVIEWDOG_GITHUB_API_TOKEN="${INPUT_GITHUB_TOKEN}"
+exit_val="0"
 
-/run-clang-format.py "$@" 2>&1 \
-    | reviewdog -name="clang-format-lint-action" -filter-mode="file" -f="diff" -reporter="github-pr-review" -fail-on-error=false -tee
+/run-clang-format.py "$@" 2>&1 | \
+    reviewdog -name="clang-format-lint-action" -f="diff" -tee \
+              -reporter="${INPUT_REPORTER}" \
+              -filter-mode="${INPUT_FILTER_MODE}" \
+              -fail-on-error="${INPUT_FAIL_ON_ERROR}" \
+              -level="${INPUT_LEVEL}" \
+              ${INPUT_REVIEWDOG_FLAGS} || exit_val="$?"
+
+if [[ "${exit_val}" -ne '0' ]]; then
+  exit 1
+fi
