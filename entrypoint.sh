782e19c5ba988004a4cc06af5d0891c4187fef18
MENGHUNG WU <m.wu@gitai.tech>
2023-01-03 15:08:39 +0900

    fail on error true

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,4 +4,4 @@ cd "$GITHUB_WORKSPACE"
 export REVIEWDOG_GITHUB_API_TOKEN="${INPUT_GITHUB_TOKEN}"
 
 /run-clang-format.py "$@" 2>&1 \
-    | reviewdog -name="clang-format-lint-action" -filter-mode="file" -f="diff" -reporter="github-pr-review" -fail-on-error -tee
+    | reviewdog -name="clang-format-lint-action" -filter-mode="file" -f="diff" -reporter="github-pr-review" -fail-on-error=true -tee
