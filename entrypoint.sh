28320c18efdb89264028b3a14aa24bfe71c971e8
MENGHUNG WU <m.wu@gitai.tech>
2023-01-03 17:50:15 +0900

    no fail on error

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 
 cd "$GITHUB_WORKSPACE"
 export REVIEWDOG_GITHUB_API_TOKEN="${INPUT_GITHUB_TOKEN}"
-exit_val="0"
+#exit_val="0"
 
 /run-clang-format.py "$@" 2>&1 | \
     reviewdog -name="clang-format-lint-action" -f="diff" -tee \
@@ -10,8 +10,9 @@ exit_val="0"
               -filter-mode="${INPUT_FILTER_MODE}" \
               -fail-on-error="${INPUT_FAIL_ON_ERROR}" \
               -level="${INPUT_LEVEL}" \
-              ${INPUT_REVIEWDOG_FLAGS} || exit_val="$?"
+              ${INPUT_REVIEWDOG_FLAGS}
+              #|| exit_val="$?"
 
-if [[ "${exit_val}" -ne '0' ]]; then
-  exit 1
-fi
+#if [[ "${exit_val}" -ne '0' ]]; then
+#  exit 1
+#fi
