008f45088a857dbc17e50807cc7e62bb1feb5c0d
MENGHUNG WU <m.wu@gitai.tech>
2023-01-03 11:14:36 +0900

    add github token

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,7 @@
 #!/bin/sh -l
 
 cd "$GITHUB_WORKSPACE"
+export REVIEWDOG_GITHUB_API_TOKEN="${INPUT_GITHUB_TOKEN}"
 
 /run-clang-format.py "$@" 2>&1 \
     | reviewdog -name="clang-format-lint-action" -filter-mode="file" -f="diff"
