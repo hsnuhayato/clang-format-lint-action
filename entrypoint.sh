7a41963e1a04c1131a08c31f10964bfe776a0002
MENGHUNG WU <m.wu@gitai.tech>
2023-01-05 10:37:05 +0900

    add tail

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,7 @@
 cd "$GITHUB_WORKSPACE"
 export REVIEWDOG_GITHUB_API_TOKEN="${INPUT_GITHUB_TOKEN}"
 
-/run-clang-format.py "$@" 2>&1 | \
+/run-clang-format.py "$@" 2>&1 |tail -n +2  \
     reviewdog -name=clang-format-lint-action -f=diff -tee \
               -reporter=github-pr-review \
               -filter-mode=nofilter \
