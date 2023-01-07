d8b1ecdeb26356026dca51a8868f695ff860e4a8
MENGHUNG WU <m.wu@gitai.tech>
2023-01-07 09:41:07 +0900

    use input args

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,8 +5,8 @@ export REVIEWDOG_GITHUB_API_TOKEN="${INPUT_GITHUB_TOKEN}"
 
 /run-clang-format.py "$@" 2>&1 |tail -n +2 | \
     reviewdog -name=clang-format-lint-action -f=diff -tee \
-              -reporter=github-pr-review \
-              -filter-mode=nofilter \
-              -fail-on-error=true \
-              -level=error \
+              -reporter="${INPUT_REPORTER}" \
+              -filter-mode="${INPUT_FILTER_MODE}" \
+              -fail-on-error="${INPUT_FAIL_ON_ERROR}" \
+              -level="${INPUT_LEVEL}" \
               ${INPUT_REVIEWDOG_FLAGS}
